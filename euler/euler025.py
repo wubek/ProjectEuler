e26ca38e3817a1ef055580e1e05429c4a3652fee
wukat <wojciechksprk@gmail.com>
2014-08-26 19:21:39 +0200

    solved

--- euler/euler025.py
+++ euler/euler025.py
@@ -2,7 +2,7 @@
 '''
 The Fibonacci sequence is defined by the recurrence relation:
 
-Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+Fn = Fn-1 + Fn-2, where F1 = 1 and F2 = 1.
 Hence the first 12 terms will be:
 
 F1 = 1
@@ -23,9 +23,21 @@ What is the first term in the Fibonacci sequence to contain 1000 digits?
 '''
 
 def fib_gen():
-    prev2, prev = 1, 1
-    pass
+    prev2, prev = 0, 1
+    yield prev
+    while True:
+        prev2 = prev + prev2
+        prev, prev2 = prev2, prev
+        yield prev
+
+def get_num_of_digits(number):
+    return len(str(number))
 
 
 if __name__ == '__main__':
-    pass
+    count = 0
+    for i in fib_gen():
+        count += 1
+        if get_num_of_digits(i) >= 1000:
+            print(count)
+            break
