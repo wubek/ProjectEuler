49a475c51e6f643e9da2e8a080ec613564f21d2d
wukat <wojciechksprk@gmail.com>
2014-08-31 23:08:58 +0200

    solved

--- euler/euler032.py
+++ euler/euler032.py
@@ -2,19 +2,39 @@
 '''
 We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1 through 5 pandigital.
 
-The product 7254 is unusual, as the identity, 39 × 186 = 7254, containing multiplicand, multiplier, and product is 1 through 9 pandigital.
+The product 7254 is unusual, as the identity, 39 x 186 = 7254, containing multiplicand, multiplier, and product is 1 through 9 pandigital.
 
 Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
 
 HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
 '''
 
-def is_pandigital(number, n):
-    return is_pandigital_str(str(number), n)
+def is_pandigital_str(str_number, n):
+    used, to_use = set(), set(str(i) for i in range(1, n + 1))
+    for digit in str_number:
+        if digit in to_use and digit not in used:
+            used.add(digit)
+        else:
+            return False
+    return not (to_use - used)
 
-def is_panadigital_str(str_number, n):
-    pass
+def find_proper_numbers():
+    proper_pairs_and_product = []
+    for first in range(2, 99):
+        second = 100
+        product = second * first
+        while product < 10000:
+            product = second * first
+            proper_pairs_and_product.append((first, second, product))
+            second += 1
+    return proper_pairs_and_product
 
+def sum_up_proper_panadigitals(proper_pairs_and_product):
+    set_of_products = set()
+    for first, second, product in proper_pairs_and_product:
+        if is_pandigital_str(str(first) + str(second) + str(product), 9):
+            set_of_products.add(product)
+    return sum(set_of_products)
 
 if __name__ == '__main__':
-    pass
+    print(sum_up_proper_panadigitals(find_proper_numbers()))
