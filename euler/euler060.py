5c013688a65f9e732dce3258e5c6dc2692e3f6bb
wukat <wojciechksprk@gmail.com>
2014-09-24 00:10:09 +0200

    faster

--- euler/euler060.py
+++ euler/euler060.py
@@ -12,20 +12,29 @@ any two primes concatenate to produce another prime.
 '''
 
 from euler050 import eratosthenes_sieve_compr
-from euler058 import is_prime
 
-def check_pair(prime1, prime2):
-    if is_prime(int(str(prime1) + str(prime2))):
-        if is_prime(int(str(prime2) + str(prime1))):
+def check_primality(num, primes):
+    limit = num ** (0.5)
+    for prime in primes:
+        if prime >= limit:
+            return True
+        if num % prime == 0:
+            return False
+    return True
+
+def check_pair(prime1, prime2, primes):
+    str1, str2 = str(prime1), str(prime2)
+    if check_primality(int(str1 + str2), primes):
+        if check_primality(int(str2 + str1), primes):
             return True
 
 def make_pairs_dict(primes):
-    dict_pairs, limit = {}, len(primes)
+    dict_pairs, limit = {}, len(primes),
     for i in primes:
         dict_pairs[i] = set()
     for i in range(limit - 1):
         for j in range(i + 1, limit):
-            if check_pair(primes[i], primes[j]):
+            if check_pair(primes[i], primes[j], primes):
                 dict_pairs[primes[i]].add(primes[j])
     return dict_pairs
 
@@ -39,6 +48,7 @@ def solve(limit):
                 for l in dict_pairs[i] & dict_pairs[j] & dict_pairs[k]:
                     for m in dict_pairs[i] & dict_pairs[j] & dict_pairs[k] & dict_pairs[l]:
                         sum_temp = i + j + k + l + m 
+                        print(i, j, k, l, m)
                         if sum_temp < min_so_far:
                             min_so_far = sum_temp
     return min_so_far
